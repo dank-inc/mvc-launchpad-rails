2e180dca141200119cf0cb9f12bc3310f418e9b3
Elijah Lucian <elijahlucian@gmail.com>
2020-10-13 18:55:34 -0600

    🤠

--- frontend/src/components/layout/AppHeader.tsx
+++ frontend/src/components/layout/AppHeader.tsx
@@ -1,7 +1,21 @@
-import { Layout } from 'antd'
+import { Col, Layout, Row, Typography } from 'antd'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-type Props = {}
-export const AppHeader = (props: Props) => {
-  return <Layout.Header></Layout.Header>
+export const AppHeader = () => {
+  return (
+    <Layout.Header className="app-header">
+      <Row justify="space-between" align="middle">
+        <Col>
+          <Typography.Title>
+            MVP MVC Boilerplate by Dank Inc. 🤠
+          </Typography.Title>
+        </Col>
+        <Col className="links">
+          <Link to="/">Home</Link>
+          <Link to="/things/cool">Cool Thing</Link>
+        </Col>
+      </Row>
+    </Layout.Header>
+  )
 }
